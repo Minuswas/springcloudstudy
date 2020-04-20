24a738c511fa6c3634ee0d4cf74bf0b814b553a9
[Minuswas] <[15594788362@189.cn]>
2020-04-20 20:38:17 +0800

    抽取api模块以及Eureka服务端

---
+++ cloud2020/cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 王佳杰
+ * @date 2020/4/20  20:32
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
